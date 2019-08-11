75b968bffffee3bda364ac2994d53a6825882943
Diego <52511665+diego989@users.noreply.github.com>
2019-08-11 17:12:25 -0300

    correcciones

--- examen nuevo/2019_2Cuatri_Parcial_fuentes/dos.js
+++ examen nuevo/2019_2Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,4 @@
-function mostrar()
+/*function mostrar()
 {
 	var edad;
 	var animal;
@@ -64,4 +64,79 @@ function mostrar()
 	
 	
 
+}*/
+/*Debemos realizar la carga de cuatro mascotas de una veterinaria, 
+de cada una debo optener los siguientes datos: 
+el tipo (validar "perro" o "gato") , 
+la edad (validar entre 1 y 25),
+el peso (validar entre 1 y 100) 
+y el nombre. 
+Se debe Informar al usuario lo siguiente: 
+a) El promedio de los pesos totales. 
+b) El nombre del perro mas viejo 
+c) La cantidad animales menores a 10 kilos de mas de 10 años.*/ 
+function mostrar()
+{
+	var animal;
+	var edad;
+	var peso;
+	var nombre;
+	var ingreso;
+	var i=0;
+	var acumulador=0;
+	var contPerro=0;
+	var nombPerro;
+	var edadPerro;
+	var contAnimales=0;
+	var promedio;
+
+
+	
+	for (i=0; i<4; i++)
+	{
+		animal = prompt("Tipo de animal");
+		while(animal!="perro" && animal!="gato")
+		{
+			animal = prompt("Error"+"\n"+"Tipo de animal");
+		}
+
+		edad = prompt("Edad del "+animal);
+		edad = parseInt(edad);
+		while (isNaN(edad) || edad<1 || edad>25)
+		{
+			edad = prompt("Error"+"\n"+"Edad del "+animal);
+			edad = parseInt(edad);
+		}
+
+		peso = prompt("Peso del "+animal);
+		peso = parseInt(peso);
+		while (isNaN(peso) || peso<1 || peso>100)
+		{
+			peso = prompt("Error"+"\n"+"Peso del "+animal);
+			peso = parseInt(peso);
+		}
+
+		nombre = prompt("Nombre del "+animal);
+
+		acumulador += peso;
+
+		if (animal =="perro")
+		{
+			contPerro++;
+			if (contPerro == 1 || edad>edadPerro)
+			{
+				edadPerro = edad;
+				nombPerro = nombre;
+			}
+		}
+
+		if (peso<10 && edad >10)
+		{
+			contAnimales++;
+		}
+	}
+
+	promedio = acumulador/i;
+
+	alert("a) El promedio de los pesos totales. "+promedio.toFixed(2)+"\n"+"b El nombre del perro mas viejo. "+nombPerro+"\n"+"c La cantidad animales menores a 10 kilos de mas de 10 años "+contAnimales);
 }
--- examen nuevo/2019_2Cuatri_Parcial_fuentes/tres.js
+++ examen nuevo/2019_2Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,4 @@
-function mostrar()
+/*function mostrar()
 {
 	var localidad;
 	var habitantes;
@@ -73,4 +73,84 @@ function mostrar()
 	document.write("c) La cantidad localidades que superan los 40 grados de temperatura es: "+ contadorTemp+ "<br>");
 	document.write("d) El promedio de habitantes entre las localidades ingresadas es: "+ (acumulador/contador)+ "<br>");
 	document.write("f) La temperatura mínima ingresada es: "+ temperaturaMin+ "<br>"+" y nombre de la localidad que registro esa temperatura es: "+ localidadTempMin);
+}*/
+
+/*Realizar el algoritmo que permita ingresar el nombre de una localidad,
+ cantidad de habitantes en millones entre 1 y 40 (validar) 
+la temperaruta mínima que se registra en esa localidad(entre -50 y 50) 
+hasta que el usuario quiera e informar al terminar el ingreso por document.write: 
+a) La cantidad de temperaturas pares. 
+b) El nombre de la localidad con menos habitantes 
+c) La cantidad localidades que superan los 40 grados de temperatura. 
+d) El promedio de habitantes entre las localidades ingresadas	
+f) La temperatura mínima ingresada, y nombre de la localidad que registro esa temperatura. */
+
+function mostrar()
+{
+	var localidad;
+	var cantHabitantes;
+	var temperatura;
+	var inicio;
+	var tempPares=0;
+	var i=0;
+	var nombreMenorHab;
+	var localidadMenorHab;
+	var acumulador=0;
+	var promedio;
+	var tempMin;
+	var localidadTempMin;
+	var contadorTemp=0;
+	
+	do
+	{
+		localidad = prompt("Nombre de la localidad");
+
+		cantHabitantes = prompt("Cantidad de habitantes de "+localidad);
+		cantHabitantes = parseInt(cantHabitantes);
+		while(isNaN(cantHabitantes) || (cantHabitantes<1 || cantHabitantes>40))
+		{
+			cantHabitantes = prompt("Error"+"\n"+"Cantidad de habitantes de "+localidad);
+			cantHabitantes = parseInt(cantHabitantes);
+		}
+
+		temperatura = prompt("Temperatura mínima de la localidad de "+localidad);
+		temperatura = parseInt(temperatura);
+		while(isNaN(temperatura) || (temperatura<-50 || temperatura>50))
+		{
+			temperatura = prompt("Error"+"\n"+"Temperatura mínima de la localidad de "+localidad);
+			temperatura = parseInt(temperatura);
+		}
+
+		if (temperatura %2 == 0)
+		{
+			tempPares++;
+		}
+
+		if (i==0 || cantHabitantes<localidadMenorHab)
+		{
+			nombreMenorHab = localidad;
+			localidadMenorHab = cantHabitantes;
+		}
+
+		if (temperatura>40)
+		{
+			contadorTemp++;
+		}
+		acumulador += cantHabitantes;
+
+		if (i==0 || temperatura<tempMin)
+		{
+			localidadTempMin = localidad;
+			tempMin = temperatura;
+		}
+
+		i++;
+
+		inicio = prompt("Si desea salir presiones n");
+	}
+	while (inicio!="n");
+
+	promedio = acumulador/i;
+
+	alert("a) La cantidad de temperaturas pares. "+tempPares+"\n"+"b) El nombre de la localidad con menos habitantes "+nombreMenorHab+"\n"+"c) La cantidad localidades que superan los 40 grados de temperatura. "+contadorTemp+"\n"+"d) El promedio de habitantes entre las localidades ingresadas "+promedio.toFixed(2)+"\n"+"f)1° La temperatura mínima ingresada, "+tempMin+"\n"+"2° nombre de la localidad que registro esa temperatura. "+localidadTempMin);
 }
